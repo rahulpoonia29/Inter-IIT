d974d769d5c1199ec56deea8252fe2d317241462
Rahul <chattychidiya@gmail.com>
2024-10-16 00:42:14 +0530

    Increace timeout to 10s

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL + "/api/v1",
-    timeout: 1000,
+    timeout: 10000,
     headers: { "Content-Type": "application/json" },
 });
 
